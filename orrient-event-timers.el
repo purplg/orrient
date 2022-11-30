63de637ae3e42e0ff19b4a33ceece64af47dc57c
Ben Whitley <dev@purplg.com>
2022-11-29 22:07:31 -0500

    Widgets and colors

--- orrient-event-timers.el
+++ orrient-event-timers.el
@@ -12,6 +12,7 @@
 ;;; Code:
 (require 'cl-lib)
 (require 'generator)
+(require 'wid-edit)
 
 (defvar orrient-timers-buffer "*orrient-event-timers*")
 
@@ -34,15 +35,16 @@
 
 (cl-defstruct orrient-timers-meta
   name
+  category
   events)
 
 (defvar orrient-timers-schedule
-  `(,(make-orrient-timers-meta :name "Day and Night"
+  `(,(make-orrient-timers-meta :name "Day and Night" :category 'core-tyria
       :events `(,(make-orrient-timers-event :name "Dawn"  :offset  25 :frequency 120 :length  5)
                 ,(make-orrient-timers-event :name "Day"   :offset  30 :frequency 120 :length 70)
                 ,(make-orrient-timers-event :name "Dusk"  :offset 100 :frequency 120 :length  5)
                 ,(make-orrient-timers-event :name "Night" :offset 105 :frequency 120 :length 40)))
-    ,(make-orrient-timers-meta :name "World Bosses"
+    ,(make-orrient-timers-meta :name "World Bosses" :category 'core-tyria
       :events `(,(make-orrient-timers-event :name "Admiral Taidha Covington" :offset   0 :frequency 180 :length 15)
                 ,(make-orrient-timers-event :name "Svanir Shaman Chief"      :offset  15 :frequency 120 :length 15)
                 ,(make-orrient-timers-event :name "Megadestroyer"            :offset  30 :frequency 180 :length 15)
@@ -53,7 +55,7 @@
                 ,(make-orrient-timers-event :name "Shadow Behemoth"          :offset 105 :frequency 120 :length 15)
                 ,(make-orrient-timers-event :name "Golem Mark II"            :offset 120 :frequency 180 :length 15)
                 ,(make-orrient-timers-event :name "Claw of Jormag"           :offset 150 :frequency 180 :length 15)))
-    ,(make-orrient-timers-meta :name "Hard World Bosses"
+    ,(make-orrient-timers-meta :name "Hard World Bosses" :category 'core-tyria
       :events `(,(make-orrient-timers-event :name "Tequatl the Sunless" :offset    0 :frequency 1440 :length 30)
                 ,(make-orrient-timers-event :name "Triple Trouble"      :offset   60 :frequency 1440 :length 30)
                 ,(make-orrient-timers-event :name "Karka Queen"         :offset  120 :frequency 1440 :length 30)
@@ -72,32 +74,106 @@
                 ,(make-orrient-timers-event :name "Tequatl the Sunless" :offset 1140 :frequency 1440 :length 30)
                 ,(make-orrient-timers-event :name "Triple Trouble"      :offset 1200 :frequency 1440 :length 30)
                 ,(make-orrient-timers-event :name "Karka Queen"         :offset 1380 :frequency 1440 :length 30)))
-    ,(make-orrient-timers-meta :name "Ley-Line Anomaly"
+    ,(make-orrient-timers-meta :name "Ley-Line Anomaly" :category 'core-tyria
       :events `(,(make-orrient-timers-event :name "Timberline Falls" :offset  20 :frequency 360 :length 20)
                 ,(make-orrient-timers-event :name "Iron Marches"     :offset 140 :frequency 360 :length 20)
                 ,(make-orrient-timers-event :name "Gendarran Fields" :offset 260 :frequency 360 :length 20)))
-    ,(make-orrient-timers-meta :name "PVP Tournaments"
+    ,(make-orrient-timers-meta :name "PVP Tournaments" :category 'core-tyria
       :events `(,(make-orrient-timers-event :name "Balthazar's Brawl"  :offset   0 :frequency 720 :length 60)
                 ,(make-orrient-timers-event :name "Grenth's Game"      :offset 180 :frequency 720 :length 60)
                 ,(make-orrient-timers-event :name "Melandru's Matchup" :offset 360 :frequency 720 :length 60)
                 ,(make-orrient-timers-event :name "Lyssa's Legions"    :offset 540 :frequency 720 :length 60)))
-    ,(make-orrient-timers-meta :name "Eye of the North"
-      :events `(,(make-orrient-timers-event :name "Twisted Marionette (Public)"     :offset  0 :frequency 120 :length 20)
-                ,(make-orrient-timers-event :name "Battle For Lion's Arch (Public)" :offset 30 :frequency 120 :length 15)
-                ,(make-orrient-timers-event :name "Tower of Nightmares (Public)"    :offset 90 :frequency 120 :length 15)))
-    ,(make-orrient-timers-meta :name "Dry Top"
+    ,(make-orrient-timers-meta :name "Eye of the North" :category 'living-world-1
+      :events `(,(make-orrient-timers-event :name "Twisted Marionette"     :offset  0 :frequency 120 :length 20)
+                ,(make-orrient-timers-event :name "Battle For Lion's Arch" :offset 30 :frequency 120 :length 15)
+                ,(make-orrient-timers-event :name "Tower of Nightmares"    :offset 90 :frequency 120 :length 15)))
+    ,(make-orrient-timers-meta :name "Scarlets Invasion" :category 'living-world-1
+      :events `(,(make-orrient-timers-event :name "TODO" :offset  0 :frequency 120 :length 20)))
+    ,(make-orrient-timers-meta :name "Dry Top" :category 'living-world-2
       :events `(,(make-orrient-timers-event :name "Crash Site" :offset  0 :frequency 60 :length 40)
                 ,(make-orrient-timers-event :name "Sandstore"  :offset 40 :frequency 60 :length 20)))
-    ,(make-orrient-timers-meta :name "Verdant Brink"
+    ,(make-orrient-timers-meta :name "Verdant Brink" :category 'heart-of-thorns
       :events `(,(make-orrient-timers-event :name "Night: Night and the Enemy"  :offset 105 :frequency 120 :length 25)
                 ,(make-orrient-timers-event :name "Night Bosses"                :offset  10 :frequency 120 :length 20)
                 ,(make-orrient-timers-event :name "Day: Securing Verdant Brink" :offset  30 :frequency 120 :length 75)))
-    ,(make-orrient-timers-meta :name "Auric Basin"
+    ,(make-orrient-timers-meta :name "Auric Basin" :category 'heart-of-thorns
       :events `(,(make-orrient-timers-event :name "Challenges" :offset 45 :frequency 120 :length 15)
                 ,(make-orrient-timers-event :name "Octovine"   :offset 60 :frequency 120 :length 20)
                 ,(make-orrient-timers-event :name "Reset"      :offset 80 :frequency 120 :length 10)
                 ,(make-orrient-timers-event :name "Pylons"     :offset 90 :frequency 120 :length 75))))
   "List of meta events.")
+
+(defvar orrient--timers-heading-length nil)
+
+
+;; Faces
+(defface orrient-timers-meta
+  '((t (:weight bold)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-core-tyria
+  '((t (:background "#7b0418" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-living-world-1
+  '((t (:background "#7b0418" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-living-world-2
+  '((t (:background "#5c4a03" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-heart-of-thorns
+  '((t (:background "#515c03" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-living-world-3
+  '((t (:background "#4b7f40" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-path-of-fire
+  '((t (:background "#7b4704" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-living-world-4
+  '((t (:background "#662a77" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-icebrood-saga
+  '((t (:background "#04497b" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-category-end-of-dragons
+  '((t (:background "#0b6b75" :inherit 'orrient-timers-meta)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defface orrient-timers-event
+  '((t (:inherit outline-1)))
+  "Face for meta heading in the event timers buffer."
+  :group 'orrient)
+
+(defun orrient--timers-get-category-face (category)
+  (pcase category
+    ('core-tyria 'orrient-timers-category-core-tyria)
+    ('living-world-1 'orrient-timers-category-living-world-1)
+    ('living-world-2 'orrient-timers-category-living-world-2)
+    ('heart-of-thorns 'orrient-timers-category-heart-of-thorns)
+    ('living-world-3 'orrient-timers-category-living-world-3)
+    ('path-of-fire 'orrient-timers-category-path-of-fire)
+    ('living-world-4 'orrient-timers-category-living-world-4)
+    ('icebrood-saga 'orrient-timers-category-icebrood-saga)
+    ('end-of-dragons 'orrient-timers-category-end-of-dragons)))
+
 
 
 ;; Event prediction
@@ -147,31 +223,49 @@
 (define-widget 'orrient-timers-event 'push-button
   "")
 
+(define-widget 'orrient-timers-meta 'item
+  "")
+
+(defun orrient-timers-meta-widget-create (widget)
+  (let ((meta (widget-get widget :meta)))
+    ;; (widget-put widget :action (orrient-timers-open-meta meta)) ; TODO Add new buffer for more detailed information on meta
+    (widget-put widget :tag (propertize (format (format "%%%ss " (orrient--timers-heading-length))
+                                                (orrient-timers-meta-name meta))
+                                        'face (orrient--timers-get-category-face (orrient-timers-meta-category meta))))
+    (widget-put widget :format "%t"))
+  (widget-default-create widget))
+
 
 ;; Rendering
 (defun orrient--timers-heading-length ()
-  (let ((name-lengths (mapcar (lambda (meta)
-                                (cons meta (length (orrient-timers-meta-name meta))))
-                              orrient-timers-schedule)))
-    (cdr (seq-reduce (lambda (left right)
-                       (if (and left
-                                (> (cdr left)
-                                   (cdr right)))
-                           left
-                         right))
-                     name-lengths
-                     nil))))
+  (or orrient--timers-heading-length
+      (setq orrient--timers-heading-length
+            (let ((name-lengths (mapcar (lambda (meta)
+                                          (cons meta (length (orrient-timers-meta-name meta))))
+                                        orrient-timers-schedule)))
+              (cdr (seq-reduce (lambda (left right)
+                                 (if (and left
+                                          (> (cdr left)
+                                             (cdr right)))
+                                     left
+                                   right))
+                               name-lengths
+                               nil))))))
 
 (defun orrient--timers-draw-meta (meta)
-  (insert (format "%s | " (orrient-timers-meta-name meta)))
+  (widget-create 'orrient-timers-meta
+                 :meta meta
+                 :create #'orrient-timers-meta-widget-create)
   (let ((iter (orrient--timers-meta-iter meta (orrient--timers-current-time))))
-    (cl-loop for i upto 5
-             do (orrient--timers-draw-event (car (iter-next iter)))))
-  (insert "\n\n"))
+    (cl-loop repeat 5 do
+             (insert " ")
+             (orrient--timers-draw-event (car (iter-next iter)))))
+  (insert "\n"))
 
 (defun orrient--timers-draw-event (event)
   (widget-create 'orrient-timers-event
-                 :tag (orrient-timers-event-name event)))
+                 :tag (orrient-timers-event-name event)
+                 :format "%t"))
 
 (defun orrient--timers-render-buffer ()
   (interactive)
@@ -179,7 +273,8 @@
     (let ((inhibit-read-only t))
       (erase-buffer)
       (dolist (meta orrient-timers-schedule)
-        (orrient--timers-draw-meta meta)))))
+        (orrient--timers-draw-meta meta)))
+    (orrient-timers-mode)))
 
 (define-derived-mode orrient-timers-mode special-mode "GW2 Event Timers"
   "View Guild Wars 2 Event Timers."
