f3f750aa8ed939ea6086a46e479f7070fa2b4d68
Ben Whitley <dev@purplg.com>
2022-12-04 12:10:40 -0500

    Renamed `orrient-timers-meta` struct to `orrient-meta`

--- orrient-event-timers.el
+++ orrient-event-timers.el
@@ -63,18 +63,18 @@ time.")
   length
   frequency)
 
-(cl-defstruct orrient-timers-meta
+(cl-defstruct orrient-meta
   name
   category
   events)
 
 (defvar orrient-timers-schedule
-  `(,(make-orrient-timers-meta :name "Day and Night" :category 'core-tyria
+  `(,(make-orrient-meta :name "Day and Night" :category 'core-tyria
       :events `(,(make-orrient-event :name "Dawn"  :offset '(0 25) :frequency '(2 0) :length '(0  5))
                 ,(make-orrient-event :name "Day"   :offset '(0 30) :frequency '(2 0) :length '(1 10))
                 ,(make-orrient-event :name "Dusk"  :offset '(1 40) :frequency '(2 0) :length '(0  5))
                 ,(make-orrient-event :name "Night" :offset '(1 45) :frequency '(2 0) :length '(0 40))))
-    ,(make-orrient-timers-meta :name "World Bosses" :category 'core-tyria
+    ,(make-orrient-meta :name "World Bosses" :category 'core-tyria
       :events `(,(make-orrient-event :name "Admiral Taidha Covington" :offset '(0  0) :frequency '(3 0) :length '(0 15))
                 ,(make-orrient-event :name "Svanir Shaman Chief"      :offset '(0 15) :frequency '(2 0) :length '(0 15))
                 ,(make-orrient-event :name "Megadestroyer"            :offset '(0 30) :frequency '(3 0) :length '(0 15))
@@ -85,7 +85,7 @@ time.")
                 ,(make-orrient-event :name "Shadow Behemoth"          :offset '(1 45) :frequency '(2 0) :length '(0 15))
                 ,(make-orrient-event :name "Golem Mark II"            :offset '(2  0) :frequency '(3 0) :length '(0 15))
                 ,(make-orrient-event :name "Claw of Jormag"           :offset '(2 30) :frequency '(2 0) :length '(0 15))))
-    ,(make-orrient-timers-meta :name "Hard World Bosses" :category 'core-tyria
+    ,(make-orrient-meta :name "Hard World Bosses" :category 'core-tyria
       :events `(,(make-orrient-event :name "Tequatl the Sunless" :offset '( 0 0) :frequency '(24 0) :length '(0 30))
                 ,(make-orrient-event :name "Triple Trouble"      :offset '( 1 0) :frequency '(24 0) :length '(0 30))
                 ,(make-orrient-event :name "Karka Queen"         :offset '( 2 0) :frequency '(24 0) :length '(0 30))
@@ -104,47 +104,47 @@ time.")
                 ,(make-orrient-event :name "Tequatl the Sunless" :offset '(19 0) :frequency '(24 0) :length '(0 30))
                 ,(make-orrient-event :name "Triple Trouble"      :offset '(20 0) :frequency '(24 0) :length '(0 30))
                 ,(make-orrient-event :name "Karka Queen"         :offset '(23 0) :frequency '(24 0) :length '(0 30))))
-    ,(make-orrient-timers-meta :name "Ley-Line Anomaly" :category 'core-tyria
+    ,(make-orrient-meta :name "Ley-Line Anomaly" :category 'core-tyria
       :events `(,(make-orrient-event :name "Timberline Falls" :offset '(0 20) :frequency '(6 0) :length '(0 20))
                 ,(make-orrient-event :name "Iron Marches"     :offset '(2 20) :frequency '(6 0) :length '(0 20))
                 ,(make-orrient-event :name "Gendarran Fields" :offset '(4 20) :frequency '(6 0) :length '(0 20))))
-    ,(make-orrient-timers-meta :name "PVP Tournaments" :category 'core-tyria
+    ,(make-orrient-meta :name "PVP Tournaments" :category 'core-tyria
       :events `(,(make-orrient-event :name "balthazar's brawl"  :offset '(0 0) :frequency '(12 0) :length '(1 0))
                 ,(make-orrient-event :name "grenth's game"      :offset '(3 0) :frequency '(12 0) :length '(1 0))
                 ,(make-orrient-event :name "melandru's matchup" :offset '(6 0) :frequency '(12 0) :length '(1 0))
                 ,(make-orrient-event :name "Lyssa's Legions"    :offset '(9 0) :frequency '(12 0) :length '(1 0))))
-    ,(make-orrient-timers-meta :name "Eye of the North" :category 'living-world-1
+    ,(make-orrient-meta :name "Eye of the North" :category 'living-world-1
       :events `(,(make-orrient-event :name "Twisted Marionette"     :offset '(0  0) :frequency '(2 0) :length '(0 20))
                 ,(make-orrient-event :name "Battle For Lion's Arch" :offset '(0 30) :frequency '(2 0) :length '(0 15))
                 ,(make-orrient-event :name "Tower of Nightmares"    :offset '(1 30) :frequency '(2 0) :length '(0 15))))
-    ,(make-orrient-timers-meta :name "Scarlets Invasion" :category 'living-world-1
+    ,(make-orrient-meta :name "Scarlets Invasion" :category 'living-world-1
       :events `(,(make-orrient-event :name "TODO" :offset '(0  0) :frequency '(2 0) :length '(0 20))))
-    ,(make-orrient-timers-meta :name "Dry Top" :category 'living-world-2
+    ,(make-orrient-meta :name "Dry Top" :category 'living-world-2
       :events `(,(make-orrient-event :name "Crash Site" :offset '(0  0) :frequency '(1 0) :length '(0 40))
                 ,(make-orrient-event :name "Sandstorm"  :offset '(0 40) :frequency '(1 0) :length '(0 20))))
-    ,(make-orrient-timers-meta :name "Verdant Brink" :category 'heart-of-thorns
+    ,(make-orrient-meta :name "Verdant Brink" :category 'heart-of-thorns
       :events `(,(make-orrient-event :name "Night: Night and the Enemy"  :offset '(1 45) :frequency '(2 0) :length '(0 25))
                 ,(make-orrient-event :name "Night Bosses"                :offset '(0 10) :frequency '(2 0) :length '(0 20))
                 ,(make-orrient-event :name "Day: Securing Verdant Brink" :offset '(0 30) :frequency '(2 0) :length '(1 15))))
-    ,(make-orrient-timers-meta :name "Auric Basin" :category 'heart-of-thorns
+    ,(make-orrient-meta :name "Auric Basin" :category 'heart-of-thorns
       :events `(,(make-orrient-event :name "Challenges" :offset '(0 45) :frequency '(2 0) :length '(0 15))
                 ,(make-orrient-event :name "Octovine"   :offset '(1  0) :frequency '(2 0) :length '(0 20))
                 ,(make-orrient-event :name "Reset"      :offset '(1 20) :frequency '(2 0) :length '(0 10))
                 ,(make-orrient-event :name "Pylons"     :offset '(1 30) :frequency '(2 0) :length '(1 15))))
-    ,(make-orrient-timers-meta :name "Tangled Depths" :category 'heart-of-thorns
+    ,(make-orrient-meta :name "Tangled Depths" :category 'heart-of-thorns
       :events `(,(make-orrient-event :name "Prep"              :offset '(0 25) :frequency '(2 0) :length '(0  5))
                 ,(make-orrient-event :name "Chak Gerent"       :offset '(0 30) :frequency '(2 0) :length '(0 20))
                 ,(make-orrient-event :name "Help the Outposts" :offset '(0 50) :frequency '(2 0) :length '(1 35))))
-    ,(make-orrient-timers-meta :name "Dragons Stand" :category 'heart-of-thorns
+    ,(make-orrient-meta :name "Dragons Stand" :category 'heart-of-thorns
       :events `(,(make-orrient-event :name "Dragon's Stand" :offset '(0 90) :frequency '(2 0) :length '(2 0))))
-    ,(make-orrient-timers-meta :name "Lake Doric" :category 'living-world-3
+    ,(make-orrient-meta :name "Lake Doric" :category 'living-world-3
       :events `(,(make-orrient-event :name "Noran's Homestead" :offset '(0 30) :frequency '(2 0) :length '(0 30))
                 ,(make-orrient-event :name "Saidra's Haven"    :offset '(1  0) :frequency '(2 0) :length '(0 45))
                 ,(make-orrient-event :name "New Loamhurst"     :offset '(1 45) :frequency '(2 0) :length '(0 45))))
-    ,(make-orrient-timers-meta :name "Crystal Oasis" :category 'path-of-fire
+    ,(make-orrient-meta :name "Crystal Oasis" :category 'path-of-fire
       :events `(,(make-orrient-event :name "Rounds 1 to 3" :offset '(0  5) :frequency '(2 0) :length '(0 10))
                 ,(make-orrient-event :name "Pinata/Reset"  :offset '(0 20) :frequency '(2 0) :length '(0 10))))
-    ,(make-orrient-timers-meta :name "Desert Highlands" :category 'path-of-fire
+    ,(make-orrient-meta :name "Desert Highlands" :category 'path-of-fire
       :events `(,(make-orrient-event :name "Buried Treasure" :offset '(0 5) :frequency '(2 0) :length '(0 10)))))
   "List of meta events.")
 
@@ -247,7 +247,7 @@ time.")
 it's next occurance from UTC 0."
   (let ((event-times (mapcar (lambda (event)
                                (cons event (orrient--timers-event-next event time)))
-                             (orrient-timers-meta-events meta))))
+                             (orrient-meta-events meta))))
     (seq-reduce (lambda (a b)
                   (if (and a
                        (< (cdr a) (cdr b)))
@@ -258,7 +258,7 @@ it's next occurance from UTC 0."
 
 (iter-defun orrient--timers-meta-iter (meta time)
   "Yields a cons of a orrient-event to it's next start time."
-  (let ((events (orrient-timers-meta-events meta)))
+  (let ((events (orrient-meta-events meta)))
     (while t
       (let* ((upcoming-events (seq-map
                               (lambda (event)
@@ -286,8 +286,8 @@ it's next occurance from UTC 0."
   (let ((meta (widget-get widget :meta)))
     ;; (widget-put widget :action (orrient-timers-open-meta meta)) ; TODO Add new buffer for more detailed information on meta
     (widget-put widget :tag (propertize (format (format "%%%ss " (1+ (orrient--timers-heading-length)))
-                                                (orrient-timers-meta-name meta))
-                                        'face (orrient--timers-get-category-face (orrient-timers-meta-category meta)))))
+                                                (orrient-meta-name meta))
+                                        'face (orrient--timers-get-category-face (orrient-meta-category meta)))))
   (widget-default-create widget))
 
 (define-widget 'orrient-timers-event 'push-button
@@ -336,7 +336,7 @@ it's next occurance from UTC 0."
   (or orrient--timers-heading-length
       (setq orrient--timers-heading-length
             (let ((name-lengths (mapcar (lambda (meta)
-                                          (cons meta (length (orrient-timers-meta-name meta))))
+                                          (cons meta (length (orrient-meta-name meta))))
                                         orrient-timers-schedule)))
               (cdr (seq-reduce (lambda (left right)
                                  (if (and left
